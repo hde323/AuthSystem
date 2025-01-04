6bf30aac5d7fe3c79cb90c59358a19ba0177584e
hadi <hadi@DESKTOP-7DT2A7I>
2025-01-04 00:36:49 -0800

    Created models for user and password reset tokens

---
+++ AuthSystem/Models/PasswordResetTokens.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthSystem.Models
+{
+    public class PasswordResetTokens
+    {
+        public PasswordResetTokens() 
+        {
+            this.ExperationTime = DateTime.Now.AddMinutes(5); 
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Tocken {  get; set; }
+        public DateTime ExperationTime { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
---
+++ AuthSystem/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthSystem.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool EmailConfirmed { get; set; } = false;
+        public ICollection<PasswordResetTokens> PasswordResetTokens { get; set; }
+
+    }
+}
